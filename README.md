6452067849ee03e648aa6e9d2406bfa8f677f695
Sampo Kääriäinen <sampo.kaariainen@gmail.com>
2021-08-19 11:28:28 +0300

    tuotantoversio lisätty

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FullStack-HY-osa3
+
+<https://polar-lake-93438.herokuapp.com/>
--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express()
 morgan.token('req-body', req => JSON.stringify(req.body))
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
-
 
 let persons = [
     {
